158cc84baaeb974ff2f2bbe169359318d7e5ea49
Sumit <60140415+Sumitlovanshi@users.noreply.github.com>
2023-03-22 23:30:29 +0530

    Delete Mapup-Assignment directory

--- Mapup-Assignment/routes/map/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const mapController = require('./controller/mapController');
-const {authenticate} = require('../auth/middleware/authenticate');
-
-router.post('/findIntersection',authenticate,mapController.findIntersection);
-
-
-module.exports = router;
